479e8bf08470cf6a56265ef396489c0a1dbda4a1
pedro <pete@hdlguy.com>
2015-09-09 16:02:30 -0600

    bringing sdk stuff

---
+++ sdk/spi_test/Debug/src/xspi_low_level_example.d
@@ -0,0 +1,29 @@
+src/xspi_low_level_example.o src/xspi_low_level_example.d: \
+ ../src/xspi_low_level_example.c \
+ ../../standalone_bsp_0/microblaze_0/include/xparameters.h \
+ ../../standalone_bsp_0/microblaze_0/include/xstatus.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_types.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_assert.h \
+ ../../standalone_bsp_0/microblaze_0/include/xspi_l.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_io.h \
+ ../../standalone_bsp_0/microblaze_0/include/mb_interface.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_exception.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_printf.h
+
+../../standalone_bsp_0/microblaze_0/include/xparameters.h:
+
+../../standalone_bsp_0/microblaze_0/include/xstatus.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_types.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_assert.h:
+
+../../standalone_bsp_0/microblaze_0/include/xspi_l.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_io.h:
+
+../../standalone_bsp_0/microblaze_0/include/mb_interface.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_exception.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_printf.h:
---
+++ sdk/uart_test/Release/src/xuartlite_low_level_example.d
@@ -0,0 +1,29 @@
+src/xuartlite_low_level_example.o src/xuartlite_low_level_example.d: \
+ ../src/xuartlite_low_level_example.c \
+ ../../standalone_bsp_0/microblaze_0/include/xparameters.h \
+ ../../standalone_bsp_0/microblaze_0/include/xstatus.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_types.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_assert.h \
+ ../../standalone_bsp_0/microblaze_0/include/xuartlite_l.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_io.h \
+ ../../standalone_bsp_0/microblaze_0/include/mb_interface.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_exception.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_printf.h
+
+../../standalone_bsp_0/microblaze_0/include/xparameters.h:
+
+../../standalone_bsp_0/microblaze_0/include/xstatus.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_types.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_assert.h:
+
+../../standalone_bsp_0/microblaze_0/include/xuartlite_l.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_io.h:
+
+../../standalone_bsp_0/microblaze_0/include/mb_interface.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_exception.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_printf.h:
