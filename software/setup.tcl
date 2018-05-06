64fe6f74543e1569e4a6279b17ab78443a063717
hdlguy <pedro@hdlguy.com>
2018-05-06 17:07:48 -0600

    software/setup.tcl comments

--- software/setup.tcl
+++ software/setup.tcl
@@ -13,21 +13,18 @@ set os "standalone"
 set application "hello1"
 
 # Create workspace and import the project into
-# If non-empty 'Error setting workspace'
 setws $sdk_dir
 
-# If non-empty 'Error creating hardware project'
 createhw -name $hwproject -hwspec $hwspec
 
-# If non-empty 'Error creating board support package'
 createbsp -name $bsp -hwproject $hwproject -proc $proc -os $os
 
 # Update the microprocessor software spec (MSS) and regenerate the BSP
 updatemss -mss $sdk_dir/$bsp/system.mss
 regenbsp -bsp $bsp
 
-# Create new application project as Empty Application (use repo -apps) to see available
-# applications for each architecture - here you need to specify everything I think
+# Create new application project as Empty Application 
+# Note that previously created source files will not be overwritten.
 createapp -name $application -app {Empty Application} -proc $proc -hwproject $hwproject -bsp $bsp -os $os
 
 # Clean and build all projects
