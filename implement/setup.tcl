1d47dde2e8124536655ae893b7e321646d7816c1
hdlguy <pedro@hdlguy.com>
2018-05-06 17:01:17 -0600

    sdk project created from script, compiles without error in SDK 2018.1

--- implement/setup.tcl
+++ implement/setup.tcl
@@ -20,7 +20,7 @@ update_ip_catalog
 source ../source/system.tcl
 generate_target {synthesis implementation} [get_files ./proj.srcs/sources_1/bd/system/system.bd]
 set_property synth_checkpoint_mode None    [get_files ./proj.srcs/sources_1/bd/system/system.bd]
-write_hwdef -force  -file ../sdk/top.hdf
+write_hwdef -force  -file ./results/top.hdf
 
 
 # add the max_rx_if_fifo
