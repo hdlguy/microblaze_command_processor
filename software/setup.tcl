5433809cfeb1d7b6b0c4ad25ec3b00edc2be910d
hdlguy <pedro@hdlguy.com>
2018-05-15 18:54:02 -0600

    command_processor software project builds.

--- software/setup.tcl
+++ software/setup.tcl
@@ -1,16 +1,17 @@
 set proj_name command_processor
 set sdk_dir ./$proj_name.sdk
 
-file delete -force $sdk_dir/.metadata
-file delete -force $sdk_dir/hw_platform_0
-file delete -force $sdk_dir/standalone_bsp_0
 
 set hwproject "hw_platform_0"
 set hwspec ../implement/results/top.hdf
 set bsp "standalone_bsp_0"
 set proc "microblaze_0"
 set os "standalone"
-set application "hello1"
+#set application "hello1"
+
+file delete -force $sdk_dir/.metadata
+file delete -force $sdk_dir/$hwproject
+file delete -force $sdk_dir/$bsp
 
 # Create workspace and import the project into
 setws $sdk_dir
@@ -23,9 +24,10 @@ createbsp -name $bsp -hwproject $hwproject -proc $proc -os $os
 updatemss -mss $sdk_dir/$bsp/system.mss
 regenbsp -bsp $bsp
 
-# Create new application project as Empty Application 
+# Create new application projects as Empty Application 
 # Note that previously created source files will not be overwritten.
-createapp -name $application -app {Empty Application} -proc $proc -hwproject $hwproject -bsp $bsp -os $os
+createapp -name "hello1"            -app {Empty Application} -proc $proc -hwproject $hwproject -bsp $bsp -os $os
+createapp -name "command_processor" -app {Empty Application} -proc $proc -hwproject $hwproject -bsp $bsp -os $os
 
 # Clean and build all projects
 projects -clean
