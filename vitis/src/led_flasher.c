ae68deb39d129d02ca802fd19b00e38ec01c66d9
pete <peter.dudley@precisionplanting.com>
2021-09-18 14:36:27 -0400

    tried and failed to control the rgb leds

--- vitis/src/led_flasher.c
+++ vitis/src/led_flasher.c
@@ -13,12 +13,16 @@ int main(void)
 	u32* gpio_ptr = (u32*)XPAR_AXI_GPIO_0_BASEADDR;
 
 	*(gpio_ptr+XGPIO_DATA_OFFSET) = 0x00000000;
-	*(gpio_ptr+XGPIO_TRI_OFFSET) = 0xfffffff0;
+	*(gpio_ptr+XGPIO_TRI_OFFSET)  = 0xfffffff0;
+
+	*(gpio_ptr+XGPIO_DATA2_OFFSET) = 0x00000fa3;
+	*(gpio_ptr+XGPIO_TRI2_OFFSET)  = 0xfffff000;
 
 	u32 count = 0;
 	while(1) {
 		for (Delay = 0; Delay < LED_DELAY; Delay++);
 		*(gpio_ptr+XGPIO_DATA_OFFSET)  = count & LED_MASK;
+		*(gpio_ptr+XGPIO_DATA2_OFFSET) = count & RGB_MASK; // the rgb leds don't work
 		count++;
 	}
 }
