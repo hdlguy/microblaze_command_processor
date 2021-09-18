1e7b7cb088666c868dc158222116c7fa737f96d6
pete <peter.dudley@precisionplanting.com>
2021-09-18 08:59:21 -0400

    cleaned led_flasher and ported command_proc to Vitis

--- vitis/setup.tcl
+++ vitis/setup.tcl
@@ -17,5 +17,8 @@ platform create -name "standalone_plat"    -hw $hw -proc $proc -os standalone
 app create -name led_flasher -platform standalone_plat -domain standalone_domain -template "Empty Application(C)"
 file link -symbolic ./workspace/led_flasher/src/led_flasher.c ../../../src/led_flasher.c
 
+app create -name command_proc -platform standalone_plat -domain standalone_domain -template "Empty Application(C)"
+file link -symbolic ./workspace/command_proc/src/command_proc.c ../../../src/command_proc.c
+
 
 
--- vitis/src/led_flasher.c
+++ vitis/src/led_flasher.c
@@ -2,61 +2,23 @@
 #include "xgpio.h"
 #include "xil_printf.h"
 
-#define LED 0x0f   /* Assumes bit 0 of GPIO is connected to an LED  */
-
-#define GPIO_EXAMPLE_DEVICE_ID  XPAR_GPIO_0_DEVICE_ID
-
 #define LED_DELAY     1000000
-
-#define LED_CHANNEL 1
-
-#ifdef PRE_2_00A_APPLICATION
-
-/*
- * The following macros are provided to allow an application to compile that
- * uses an older version of the driver (pre 2.00a) which did not have a channel
- * parameter. Note that the channel parameter is fixed as channel 1. */
-#define XGpio_SetDataDirection(InstancePtr, DirectionMask) \
-        XGpio_SetDataDirection(InstancePtr, LED_CHANNEL, DirectionMask)
-
-#define XGpio_DiscreteRead(InstancePtr) \
-        XGpio_DiscreteRead(InstancePtr, LED_CHANNEL)
-
-#define XGpio_DiscreteWrite(InstancePtr, Mask) \
-        XGpio_DiscreteWrite(InstancePtr, LED_CHANNEL, Mask)
-
-#define XGpio_DiscreteSet(InstancePtr, Mask) \
-        XGpio_DiscreteSet(InstancePtr, LED_CHANNEL, Mask)
-
-#endif
-
-XGpio Gpio; /* The Instance of the GPIO Driver */
-
+#define LED_MASK 0x000f
+#define RGB_MASK 0x0fff
 
 int main(void)
 {
-	int Status;
+
 	volatile int Delay;
+	u32* gpio_ptr = (u32*)XPAR_AXI_GPIO_0_BASEADDR;
 
-	print("initializing the XGpio driver\n\r");
-	Status = XGpio_Initialize(&Gpio, GPIO_EXAMPLE_DEVICE_ID);
-	if (Status != XST_SUCCESS) {
-		xil_printf("Gpio Initialization Failed\r\n");
-		return XST_FAILURE;
-	}
+	*(gpio_ptr+XGPIO_DATA_OFFSET) = 0x00000000;
+	*(gpio_ptr+XGPIO_TRI_OFFSET) = 0xfffffff0;
 
-	/* Set the direction for all signals as inputs except the LED output */
-	XGpio_SetDataDirection(&Gpio, LED_CHANNEL, ~LED);
-
-
-	while (1) {
-		XGpio_DiscreteWrite(&Gpio, LED_CHANNEL, LED);
-
+	u32 count = 0;
+	while(1) {
 		for (Delay = 0; Delay < LED_DELAY; Delay++);
-
-		XGpio_DiscreteClear(&Gpio, LED_CHANNEL, LED);
-
-		for (Delay = 0; Delay < LED_DELAY; Delay++);
+		*(gpio_ptr+XGPIO_DATA_OFFSET)  = count & LED_MASK;
+		count++;
 	}
-
 }
